8eb7991fd84dca5257d5c196a8c1fc502ba90078
Igor2693 <i.bugaytsov@mail.ru>
2024-08-01 22:20:43 +0300

    add const

---
+++ script.js
@@ -0,0 +1,10 @@
+const title = 'Первый урок';
+const screens = 1;
+const screenPrice = 2;
+const rollback = 3;
+const fullPrice = 4;
+const adaptive = 5;
+
+alert('Ваша заявка отправлена!');
+console.log('Внимание, внимание');
+ 
